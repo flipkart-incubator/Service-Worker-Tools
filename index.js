3ac5919fcd6957afc38672eb4ff1bdaa43b8d069
SHUBHAM GUPTA <shubhamgupta541994@gmail.com>
2018-03-08 18:04:05 +0530

    first cut for service-worker

---
+++ index.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+    module.exports = {
+        serviceWorkerGenerator: require('./lib/serviceWorkerGenerator'),
+        ServiceWorkerUtils: require('./lib/serviceWorkerUtils')
+    };
+})();
