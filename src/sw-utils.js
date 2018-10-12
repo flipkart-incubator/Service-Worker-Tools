9a4135c1ee519af305a148c95c740500ccee24dd
Mayur Arora <mayur.arora@flipkart.com>
2018-10-12 17:03:21 +0530

    Enhanced Injector API

--- src/sw-utils.js
+++ src/sw-utils.js
@@ -13,7 +13,55 @@
  * @function injectServiceWorker
  * @param {funtion} updateCallback - Callback to be triggered when service worker update is found.
  */
-function injectServiceWorker(updateCallback) {
+
+const createFrame = (id, innerHTML, classList) => {
+    const frame = document.createElement('div');
+    frame.setAttribute('id', id);
+    frame.innerHTML = innerHTML;
+    frame.classList.add(classList);
+    document.body.appendChild(frame);
+};
+
+const appUpdateTemplate = "<div id='update-txt'><img src='https://retail.flixcart.com/www/fk-retail-vpp/icon-star.png' /><span id='message'>Catch up with the latest version.</span><a id='update-app'>Update</a></span><a id='ignore-update'>X</a></div>";
+
+const offlineTemplate = "<div id='offline-txt'><img src='https://retail.flixcart.com/www/fk-retail-vpp/icon-plug.png' /><span id='message'>You are offline. All that you see could be outdated.</span></div>";
+
+const inFlightRequestsTemplate = "<div id='in-flight-requsts-txt'><img src='https://retail.flixcart.com/www/fk-retail-vpp/icon-hourglass.svg' /><span id='message'>Waiting for this page to complete ongoing requests...</span></div>";
+
+
+const hideFrame = id => document.getElementById(id).classList.add('hidden-frame');
+
+const showFrame = id => document.getElementById(id).classList.remove('hidden-frame');
+
+addEventListener('offline', () => showFrame('offline-notification-frame'));
+
+addEventListener('online', () => hideFrame('offline-notification-frame'));
+
+function updateExperience(sw) {
+    let refreshing;
+    createFrame('app-update-frame', appUpdateTemplate, ['hidden-frame']);
+    createFrame('offline-notification-frame', offlineTemplate, ['hidden-frame']);
+    createFrame('in-flight-requests-frame', inFlightRequestsTemplate, ['hidden-frame']);
+    document.getElementById('ignore-update').addEventListener('click', () => hideFrame('app-update-frame'));
+    navigator.serviceWorker.addEventListener(
+        'controllerchange',
+        () => {
+            if (refreshing) return;
+            refreshing = true;
+            window.location.reload();
+        },
+    );
+    showFrame('app-update-frame');
+    document.getElementById('update-app').addEventListener('click', () => {
+        hideFrame('app-update-frame');
+        showFrame('in-flight-requests-frame');
+        sw.postMessage({
+            type: 'SKIP-WAITING',
+        });
+    });
+}
+
+function injectServiceWorker({ defaultBehaviour = false, cb }) {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/service-worker.js')
             .then((reg) => {
@@ -22,8 +70,10 @@ function injectServiceWorker(updateCallback) {
                     newWorker.addEventListener('statechange', () => {
                         if (navigator.serviceWorker.controller && reg.waiting && newWorker.state === 'installed') {
                             const awaitingSW = reg.waiting;
-                            if (updateCallback && typeof updateCallback === 'function') {
-                                updateCallback(awaitingSW);
+                            if (!defaultBehaviour && cb && typeof cb === 'function') {
+                                cb(awaitingSW);
+                            } else if (defaultBehaviour) {
+                                updateExperience(awaitingSW);
                             }
                         }
                     });
@@ -31,13 +81,9 @@ function injectServiceWorker(updateCallback) {
             }, (err) => {
                 throw new Error('Service worker registration failed: ', err);
             });
-        navigator.serviceWorker.addEventListener('message', (event) => {
-            if (event.data === 'RELOAD') {
-                window.location.reload();
-            }
-        });
     }
 }
+
 
 /**
  * Handles service worker update.
